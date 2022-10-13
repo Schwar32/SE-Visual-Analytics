6a8b52129df9c3dc382e95440115d5b1b6de7993
Schwar32 <schwar32@students.rowan.edu>
2022-10-13 11:32:07 -0400

    Added AudioDropdown

---
+++ reactapp/src/AudioFileDropdown.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import Select from "react-select";
+
+function BirdDropdown({ bird, handleChange }) {
+  const [file, fileChange] = useState();
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    if (bird !== "") {
+      fetch("/api/bird-audio-files/" + bird)
+        .then((response) => response.json())
+        .then((data) => {
+          setOptions(
+            data.map((entry) => {
+              var singleObj = {};
+              singleObj["label"] = "Audio File " + entry;
+              singleObj["value"] = entry;
+              return singleObj;
+            })
+          );
+        });
+    }
+  }, [bird]);
+
+  return (
+    <div>
+      <Select
+        value={file}
+        options={options}
+        placeholder="Select Bird"
+        dropdownPosition="top"
+        className="select"
+        color="#5c5cff"
+        onChange={handleChange}
+        isSearchable
+      />
+    </div>
+  );
+}
+export default BirdDropdown;
