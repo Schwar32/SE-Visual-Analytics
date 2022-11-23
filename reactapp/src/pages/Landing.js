30cc262a0fc65b531657d6d127bf7575231a14a3
flashkard <sempre15@students.rowan.edu>
2022-11-23 15:44:17 -0800

    -Added Identify, and About buttons. -Made the buttons display horizontally instead of vertically. -Added intro paragraph under title.

--- reactapp/src/pages/Landing.js
+++ reactapp/src/pages/Landing.js
@@ -6,7 +6,13 @@ import { NavLink } from "react-router-dom";
 function Landing() {
   return (
     <div className="landing-page">
-      <h1 className="py-5 landing-title">Visual Analytics of Bird Calls</h1>
+      <h1 className="py-5 landing-title">Welcome to the World of Visual Analytics</h1>
+      <div className="py-5 landing-body">
+        <p> See results of graph output of multiple calls per bird, location information, and more!<br></br>
+            Presented it in an intuitive and easy to understand format.<br></br>
+            Explore hundreds of birds and compare data in real time.<br></br> 
+            Click Below to find out more.<br></br> </p>
+      </div>
       <div className="landing-globe-container">
         <SizeMe refreshRate={30}>
           {({ size: { width } }) => (
@@ -19,11 +25,25 @@ function Landing() {
             />
           )}
         </SizeMe>
+      
       </div>
-      <NavLink to="/Explore" className="landing-link">
-        Explore
-      </NavLink>
+      <div className="explore">
+        <NavLink to="/Explore" className="landing-link">
+          Explore
+        </NavLink>
+      </div>
+      <div className="bird-id">
+        <NavLink to="/BirdIdentifyer" className="landing-link">
+          Identify
+        </NavLink>
+      </div>
+      <div className="about">
+        <NavLink to="/About" className="landing-link">
+          About
+        </NavLink>
+      </div>
     </div>
+    
   );
 }
 
