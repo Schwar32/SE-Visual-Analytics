c507614ec5b051a037ec4b87d4bcd6dd677bcb12
pollo <yeison.2rodriguez@gmail.com>
2022-12-01 17:23:09 -0500

    Optimized About page with css about file.

--- reactapp/src/pages/About.js
+++ reactapp/src/pages/About.js
@@ -57,14 +57,23 @@ function About() {
           <li>and others</li>
         </ul>
       </div>
+
       <br></br>
+
+      <link rel="stylesheet" href="About.css"></link>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer"
+      />
       <div className="center">
-        <button>
-          <a
-            href="https://github.com/Schwar32/SE-Visual-Analytics.git"
-            className="fa fa-Github"
-          ></a>
-        </button>
+        <a href="https://github.com/Schwar32/SE-Visual-Analytics.git">
+          <button>
+          <i class="fa-brands fa-github-alt"></i>
+          </button>
+        </a>
       </div>
     </div>
   );
