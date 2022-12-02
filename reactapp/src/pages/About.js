05f7dcd4c5f54edfacc6c3f66cff7e2cd19af1f6
pollo <yeison.2rodriguez@gmail.com>
2022-12-02 11:08:07 -0500

    updated about page with css.

--- reactapp/src/pages/About.js
+++ reactapp/src/pages/About.js
@@ -14,7 +14,7 @@ function About() {
           able to compare and contrast different bird calls! Each bird call has
           its own set of visualizations, which you can compare and contrast the
           different bird calls. If you are looking to analyze different bird
-          calls, this is the sight to be on. There are over 400 different birds
+          calls, this is the sight to be on. There are over 350 different birds
           in our database that you can choose from (Up to three birds at once
           can be compared!). Each bird comes with their own set of graphs, a
           sound file, where the bird is found, and an image of what the selected
@@ -70,9 +70,7 @@ function About() {
       />
       <div className="center">
         <a href="https://github.com/Schwar32/SE-Visual-Analytics.git">
-          <button>
           <i class="fa-brands fa-github-alt"></i>
-          </button>
         </a>
       </div>
     </div>
